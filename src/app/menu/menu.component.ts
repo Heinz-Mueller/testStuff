1c7b77edee79096cf79f67cbfaa128fbd3a6044d
Fuse <noobomat2@gmx.de>
2021-06-14 12:32:57 +0200

    fixup! weitere super sachen2
    # Conflicts:
    #	src/app/menu/menu.component.ts

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  total = 0;
-  test = false;
+  tier: String = 'CAT';
+  noise: String = 'meow';
+
+  ja: String = 'ja';
+  nein: String = 'nein';
 
   constructor() { }
 
